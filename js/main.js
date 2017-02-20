7cd4f5cd2d9ae87f32c99836831d06d64cfebf77
Damian Fiałkiewicz <aekal4@gmail.com>
2017-02-20 22:30:58 +0100

    Add hamburger menu & new color palette

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 	//Scroll to section
-	$(".nav-link").on("click", function() {
+	$(".nav-link").on("click", function(e) {
 		var section = $(this).attr("href");
 		$("html, body").animate({
 			scrollTop: $(section).offset().top
 		}, 500);
+	});
+
+	//Cache menu
+	var $menu = $(".menu");
+
+	//Hamburger menu toggle
+	$(".hamburger").on("click", function() {
+		$menu.toggleClass("open");
+	});
+
+	//Hide mobile menu after click on linked
+	$(".nav-link").on("click", function() {
+		if ($(window).width() <= 650) {
+			$menu.toggleClass("open");
+		}
 	});
 });
