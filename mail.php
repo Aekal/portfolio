f50a72023a8257ad10b7d99cb5ce6e0c88f8336c
Damian Fiałkiewicz <aekal4@gmail.com>
2017-02-23 18:18:00 +0100

    Add status info

--- mail.php
+++ mail.php
@@ -1,14 +1,23 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-$to = "damian.fialkiewicz@gmail.com";
-$subject = "Portfolio | Kontakt";
-$name = $_POST('name');
-$email = $_POST('email');
-$content = $_POST('message');
-$message = "Od: $name \n".
-				"email: $email \n".
-				"Wiadomość: \n\n"."$message";
+if (isset($_POST['submit'])) {
 
-mail($to, $subject, $message);
+	$to = "damian.fialkiewicz@gmail.com";
+	$subject = "Portfolio | Kontakt";
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$content = $_POST['message'];
+	$message = "Od: $name \n".
+					"Email: $email \n".
+					"Wiadomość: \n\n"."\"$content\"";
+
+	if (mail($to, $subject, $message)) {
+		echo '<p>Twoja wiadomość została wysłana!</p>';
+		echo '<a href="/">Wróć do strony głownej</a>';
+	} else {
+		echo '<p>Coś poszło nie tak, cofnij i spróbuj ponownie!</p>';
+		echo '<a href="/">Wróć do strony głownej</a>';
+	};
+}
 ?>
