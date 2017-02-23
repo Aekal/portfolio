7e8d6c065a3621eb4eb248e618e6e1cb76e4d00a
Damian Fiałkiewicz <aekal4@gmail.com>
2017-02-23 19:55:10 +0100

    Add new fonts

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	//Scroll to section
 	$(".nav-link").on("click", function(e) {
 		var section = $(this).attr("href");
+		e.preventDefault();
 		$("html, body").animate({
 			scrollTop: $(section).offset().top
 		}, 500);
