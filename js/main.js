58e8191aa0954b92d442ad8a4792f0f1ee5383b0
Damian Fiałkiewicz <aekal4@gmail.com>
2017-02-28 22:49:57 +0100

    Add scroll arrow

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-	//Scroll to section
+	//Scroll on nav links click
 	$(".nav-link").on("click", function(e) {
 		var section = $(this).attr("href");
 		e.preventDefault();
+		scrollToSection(section);
+	});
+
+	//Scroll on arrow click
+	$(".scroll-arrow").on("click", function() {
+		scrollToSection("#o-mnie");
+	});
+
+	function scrollToSection(section) {
 		$("html, body").animate({
 			scrollTop: $(section).offset().top
 		}, 500);
-	});
+	}
 
 	//Cache menu
 	var $menu = $(".menu");
